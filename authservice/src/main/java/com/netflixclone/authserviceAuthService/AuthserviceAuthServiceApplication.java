148e63868d7d255e01b557634cf5755e4a0a3905
bergandara <gandaraber@gmail.com>
2025-06-02 09:07:28 +0100

    auth service added, starting with development

---
+++ authservice/src/main/java/com/netflixclone/authserviceAuthService/AuthserviceAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.netflixclone.authserviceAuthService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthserviceAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthserviceAuthServiceApplication.class, args);
+	}
+
+}
---
+++ authservice/src/test/java/com/netflixclone/authserviceAuthService/AuthserviceAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflixclone.authserviceAuthService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserviceAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
